58129e2736f8294f39b66775fd4d8fae2cc706e1
xaviguasch <xaviguasch@gmail.com>
2020-06-04 12:32:23 +0200

    added another solution to challenge 4 using high order functions

--- script.js
+++ script.js
@@ -50,13 +50,21 @@ function reverseInt(int) {
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {
-  const strArr = str.toLowerCase().split(' ')
-  console.log(strArr)
+  // const strArr = str.toLowerCase().split(' ')
+  // console.log(strArr)
 
-  for (let i = 0; i < strArr.length; i++) {
-    strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1)
-  }
-  return strArr.join(' ')
+  // for (let i = 0; i < strArr.length; i++) {
+  //   strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1)
+  // }
+  // return strArr.join(' ')
+
+  /////////////////////////////////
+
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => (word = word[0].toUpperCase() + word.substring(1)))
+    .join(' ')
 }
 
 // CHALLENGE 5: MAX CHARACTER
